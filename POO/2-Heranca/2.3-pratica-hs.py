204aef2de883df60b4ac86f4ecf038a99b0e4ec3
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-03-06 17:44:38 -0300

    pratica heranca simples

--- POO/2-Heranca/2.3-pratica-hs.py
+++ POO/2-Heranca/2.3-pratica-hs.py
@@ -7,6 +7,9 @@ class Veiculo:
     def ligar_motor(self) -> None:
         print('Ligando o motor.')
 
+    def __str__(self) -> str: # Este método especial será executado quando uma instância do objeto for printada (Stringificada)
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}" # List Comprehension
+
 class Motocicleta(Veiculo):
     pass
 
@@ -15,20 +18,24 @@ class Carro(Veiculo):
 
 class Caminhao(Veiculo):
     def __init__(self,cor, placa, numero_rodas, carregado: bool) -> None:
+        super().__init__(cor, placa, numero_rodas) # Para pegar os atributos da classe pai, utilize super() 
         self.carregado = carregado
 
     def esta_carregado(self):
         print(f'{'' if self.carregado else 'Não '}estou carregado')
 
-# moto = Motocicleta('Preta', 'BXZ-450', 2) # Classe filha herda o construtor.
+moto = Motocicleta('Preta', 'BXZ-450', 2) # Classe filha herda o construtor.
 # moto.ligar_motor() # E suas obrigações também.
-
-# carro = Carro('Branco', 'XDE-781', 4)
+carro = Carro('Branco', 'XDE-781', 4)
 # carro.ligar_motor()
+caminhao = Caminhao('Azul', 'RTE-490', 8, True)#
+# caminhao.ligar_motor()
+# caminhao.esta_carregado() 
+# print(f'A cor do caminhão é {caminhao.cor}') # Caminhão não tem atributo co
 
-caminhao = Caminhao('Azul', 'RTE-490', 8, True)
-caminhao.ligar_motor()
-caminhao.esta_carregado() #
+print(carro, '\n')
+print(moto, '\n')
+print(caminhao, '\n')
 
 # P: Dá pra usar um metódo de uma classe irmã?
 # carro.esta_carregado() R: Por questão de escopo, não!
