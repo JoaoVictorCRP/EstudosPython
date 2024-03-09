c9f48a3a2891437cd38901a6ed2c10bc427ae4d4
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-03-08 22:06:25 -0300

    encapsulamento 1

---
+++ POO/3-Encapsulamento/3.1-encapsulamento.py
@@ -0,0 +1,6 @@
+#                                       O QUE É ENCAPSULAMENTO?
+#  O encapsulamento é um dos conceitos fundamentais em POO, ele descreve a ideia de agrupar dados e os métodos que manipulam esses
+#  dados em uma unidade. Isso impõe restriuções ao acesso direto a variáveis e métodos e pode evitar a modificação acidental de dados
+#  Para evitar alterações acidentais, a variável de um objeto só pode ser alterada pelo método desse objeto.
+
+# Encapsulamento --> Proteção de dados de um objeto.
