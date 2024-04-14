89946688e72735ff172cb6c3ea93db26b0750f47
Joao Victor <joaovictorxl120@gmail.com>
2024-04-13 21:58:02 -0300

    criando documento

--- Pymongo/main.py
+++ Pymongo/main.py
@@ -1,15 +1,28 @@
-import pymongo
-from pymongo import MongoClient
+import pymongo as pm
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 # Atribuindo Connection String
 load_dotenv()
 passwd = os.getenv('MONGO_PASSWORD')
 connection = f'mongodb+srv://joaovictorxl120:{passwd}@cluster0.3rqdk7b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-
-
-client = MongoClient(connection)
-db = client.Projeto_Agenda
-collection = db.contatos
-print(collection.find_one({'nome':'Sarah'}))
+# Conectando cliente
+client = pm.MongoClient(connection)
+# Selecionando BD
+db = client.TesteBD
+# Listando coleções
+# print(db.list_collections)
+# Selecionando Coleção
+collection = db.pessoas
+# Criando um documento
+post = {
+    "nome":"Mike",
+    "idade":23,
+    "descrição":"Programador fullstack Python, especialista em Django.",
+    "tags":["python","programador","django","jovem"],
+    "date": datetime.now()
+}
+# Preparando para submeter as infos 
+post_id = collection.insert_one(post).inserted_id
+print(post_id)
